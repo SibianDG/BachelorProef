60ff0cfc2877147a5c6f64021cc67a456c2322a9
SibianDG <sibiandegussem@outlook.com>
2022-04-03 20:20:37 +0200

    Speech recognitions bug fixt

--- Python/Website/Website.py
+++ Python/Website/Website.py
@@ -15,7 +15,8 @@ app = Flask(__name__)
 app.config['UPLOAD_EXTENSIONS'] = ['.wav']
 r = sr.Recognizer()
 
-def to_chuncks(file):
+
+def speech_recognition(file):
     print(file, ' to chunks')
     AudioSegment.converter = which("ffmpeg")
     myaudio = AudioSegment.from_file(file)
@@ -55,20 +56,29 @@ def to_chuncks(file):
         print("exporting", chunk_name)
         chunk.export(chunk_name, format="flac")
 
+    DIR = './uploads/chunks/'
+
+    numberOfItems = len([name for name in os.listdir(DIR) if os.path.isfile(os.path.join(DIR, name))])
+
+    total_text = ""
+    for i in range(numberOfItems):
+        # Speech Recognition
+        audio_file = sr.AudioFile(f'./uploads/chunks/chunck{i}.flac')
+        with audio_file as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.record(source)
+            text = r.recognize_google(audio_data=audio, language="nl-BE")
+            total_text += " " + text
+            print("######## Google Recognize ####################")
+            print(text)
+            print("##############################################")
+    return total_text.strip()
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        task_content = request.form['content']
-        # new_task = Todo(content=task_content)
-
-        # try:
-        #     # db.session.add(new_task)
-        #     db.session.commit()
-        #     return redirect('/')
-        # except:
-        #     return 'There was an issue adding your task'
-
+        print("POST")
     else:
         return render_template('index.html')
 
@@ -78,31 +88,30 @@ def index():
 def receive():
     now = datetime.now()
     d1 = now.strftime("%Y%m%d%H%M%S")
-
-    # TODO: eerst in chuncks
     data = request.files['audio_data'].read()
-
     file = f'./uploads/{d1}.wav'
     with open(os.path.abspath(file), 'wb') as f:
         f.write(data)
-    to_chuncks(file)
-    audio_file = sr.AudioFile('./uploads/chunks/chunck0.flac')
-    with audio_file as source:
-        r.adjust_for_ambient_noise(source)
-        audio = r.record(source)
-        text = r.recognize_google(audio_data=audio, language="nl-BE")
-        print("######## Google Recognize ####################")
-        print(text)
-        print("##############################################")
-    response = jsonify(text)
+    try:
+        print(11111)
+        total_text = speech_recognition(file)
+    except:
+        print(22222)
+        total_text = "ERROR"
+    print("ERNA: " + total_text)
+    response = jsonify(total_text)
     response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
+
+    #return render_template('results.html', text=total_text)
     # laatste stap!
-    #if os.path.exists('./uploads/chunks'):
+    # if os.path.exists('./uploads/chunks'):
     #    os.rmdir('./uploads/chunks')
     #    if os.path.exists(file):
+
+
 #        os.remove(file)
-    return response
 
 
 if __name__ == "__main__":
