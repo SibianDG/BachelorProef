abdee6032873bee9adf4c3882a7ebabebca111d6
Sibian DG <sibiandegussem@outlook.com>
2022-04-27 12:22:39 +0200

    Testen

--- Python/Tests/test.py
+++ Python/Tests/test.py
@@ -1,11 +1,19 @@
 import json
 import os
 
+import pandas as pd
+import seaborn as sn
+import matplotlib.pyplot as plt
+
 arr = os.listdir('./files/jotform/')
 with open('test_results.json') as json_file:
     data = json.load(json_file)
 
-confusion_matrices = {"verkleinwoord": [[0, 0], [0, 0]], "pitch": [[0, 0], [0, 0]], "loudness": [[0, 0], [0, 0]]} #predicted == row
+print(data)
+print(len(data))
+
+confusion_matrices = {"verkleinwoord": [[0, 0], [0, 0]], "pitch": [[0, 0], [0, 0]],
+                      "loudness": [[0, 0], [0, 0]]}  # predicted == row
 
 for file, d in data.items():
     l = list()
@@ -30,3 +38,18 @@ for file in arr:
             m[int(file[18])][int(file[18])] += 1
             confusion_matrices[k] = m
 print(confusion_matrices)
+
+
+
+classes = ["True", "False"]
+
+for k, v in confusion_matrices.items():
+    df_cfm = pd.DataFrame(v, index=classes, columns=classes)
+    plt.figure(figsize=(10, 7))
+    plt.title(k)
+    # plt.ylabel('Voorspelde waarde', fontsize=18)
+    cfm_plot = sn.heatmap(df_cfm, annot=True)
+    cfm_plot.set_xlabel('Echte waarde')
+    cfm_plot.set_ylabel('Voorspelde waarde')
+
+    cfm_plot.figure.savefig(f"./plots/cfm_{k}.png")
